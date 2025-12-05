eb1cc0e986f14acd82cf6a65cd92371a031bb8c3
SJPBP <73006594+SJPBP@users.noreply.github.com>
2025-12-05 17:51:16 -0500

    Added comment explaining method

--- src/services/EpisodeService.py
+++ src/services/EpisodeService.py
@@ -3,21 +3,41 @@ from tables.EpisodeTable import EpisodeTable
 from tables.Database import Database
 
 class EpisodeService:
-    def __init__(self, db: Database, tvshow: str):
-        self.db = db
-        self.tvshow = tvshow
-        self.table = EpisodeTable(self.db, tvshowName=tvshow)
+    def __init__(self, db: Database):
+        """
+        Obtain episode data for tvshow
 
-    def get_episodes(self, pageUrl: str, numberOfEpisodes: int, oldShow: bool = False):
+        Parameters:
+        db (Database): Connect to database
+        """
+        self.db = db
+        
+
+    def get_episodes(self, tvshow: str, pageUrl: str, numberOfEpisodes: int, oldShow: bool = False):
+        """
+        Retrieve episode data for the selected episode number of the specified TV show.
+
+        Parameters:
+        pageUrl (str): URL of the TV show page on ApneTV.
+        numberOfEpisodes (int): The total number of episodes for which to retrieve data.
+        oldShow (bool): Indicates whether the TV show has concluded; this value is always `False`. 
+        tvshow (str): The title of the TV show for which data is being retrieved.
+        """
+        # Connect to db and use table named after tvshow 
+        table = EpisodeTable(self.db, tvshowName=tvshow)
+
         response = {}
         response["Episodes"] = []
 
-        episodes = self.table.get_all()
+        # Obtain Episode data from the database.
+        episodes = table.get_all()
 
+        # Episode data is not in the database.
         if episodes is None:
+            # Obtain the data from the website
             scraper = EpisodeScraper(pageUrl)
             episodes = scraper.get_episodes(numberOfEpisodes)
-            episodes = self.table.insert_all(episodes)
+            episodes = table.insert_all(episodes)
 
         for episode in episodes:
             episode.date = episode.convert_date_to_apnetv_format()
@@ -25,17 +45,29 @@ class EpisodeService:
 
         return response
 
-    def get_episode(self, pageUrl: str, date: str):
+    def get_episode(self, tvshow: str, pageUrl: str, date: str):
+        """
+        Retrieve episode data for a specific date of the given TV show.
+
+        Parameters:
+        tvshow (str): The title of the TV show for which data is being retrieved.
+        pageUrl (str): URL of the TV show page on ApneTV.
+        date (str): The date for which to retrieve episode data.
+        """
+        table = EpisodeTable(self.db, tvshowName=tvshow)
+
         response = {}
         response["Episodes"] = []
 
-        episode = self.table.get_by_date(date)
+        # Obtain Episode data from the database.
+        episode = table.get_by_date(date)
 
+        # Episode data is not in the database.
         if episode is None:
-            return None
+            # Obtain the data from the website
             scraper = episodeScraper(self.pageUrl)
             episode = scraper.get_episode(name)
-            self.table.insert(episode["episodes"][0])
+            table.insert(episode["episodes"][0])
 
         episode.date = episode.convert_date_to_apnetv_format()
 
