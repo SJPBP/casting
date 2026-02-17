d807bf83ddc61058cbe5e02661ebe04bf0eec350
SJPBP <73006594+SJPBP@users.noreply.github.com>
2026-02-17 10:59:41 -0500

    Adding spacing

--- src/services/EpisodeService.py
+++ src/services/EpisodeService.py
@@ -1,9 +1,10 @@
-from scrapers import EpisodeFetcher
-from scrapers.EpisodeScraper import EpisodeScraper
-from scrapers.EpisodeFetcher import EpisodeFetcher
-from tables.EpisodeTable import EpisodeTable
-from tables.Database import Database
 from classes.EPISODE import EPISODE
+from scrapers import EpisodeFetcher
+from scrapers.EpisodeFetcher import EpisodeFetcher
+from scrapers.EpisodeScraper import EpisodeScraper
+from tables.Database import Database
+from tables.EpisodeTable import EpisodeTable
+
 
 class EpisodeService:
     def __init__(self, db: Database, tvshowName: str, pageUrl: str):
@@ -16,33 +17,34 @@ class EpisodeService:
         pageUrl (str): URL of the TV show page on ApneTV.
         """
         self.db = db
-        self.tvshowName =tvshowName
+        self.tvshowName = tvshowName
         self.pageUrl = pageUrl
         self.shallow_search()
-        
+
     def shallow_search(self):
         episodeTable = EpisodeTable(self.db, tvshowName=self.tvshowName)
 
         episodeScraper = EpisodeScraper(pageUrl=self.pageUrl)
 
         latest_episode_date: list[EPISODE] = episodeTable.latest_episode()
-        
-        # Table is filled, but behind then start adding until top episode from table 
+
+        # Table is filled, but behind then start adding until top episode from table
         if latest_episode_date is not None:
-            latest_episode_date = latest_episode_date[0].convert_date_from_mysql_to_apnetv_format()
+            latest_episode_date = latest_episode_date[
+                0
+            ].convert_date_from_mysql_to_apnetv_format()
 
             print("Latest_Episode", latest_episode_date)
             episodes: list[EPISODE] = episodeScraper.shallow_search(latest_episode_date)
 
         else:
             # Find all the episodes
-            episodes: list[EPISODE] = episodeScraper.shallow_search() 
+            episodes: list[EPISODE] = episodeScraper.shallow_search()
         if episodes:
             # Add if there is something inside the list
             episodeTable.batch_insert_all(episodes)
 
         return True
-            
 
     def get_episodes(self, startNumber: int, endNumber: int, oldShow: bool = False):
         """
@@ -51,9 +53,9 @@ class EpisodeService:
         Parameters:
         startNumber (int): The starting episode number (inclusive). Defaults to 1.
         endNumber (int): The ending episode number (inclusive). Defaults to 8.
-        oldShow (bool): Indicates whether the TV show has concluded; this value is always `False`. 
+        oldShow (bool): Indicates whether the TV show has concluded; this value is always `False`.
         """
-        # Connect to db and use table named after tvshow 
+        # Connect to db and use table named after tvshow
         episodeTable = EpisodeTable(self.db, tvshowName=self.tvshowName)
 
         response = {}
@@ -76,17 +78,16 @@ class EpisodeService:
                     episodes[index] = result
 
                 episode.date = episode.convert_date_from_mysql_to_apnetv_format()
-            
+
             if episodes:
                 # Add if there is something inside the list
                 episodeTable.batch_insert_all(episodes)
 
             response["Episodes"].append(episodes)
 
-
         return response
-        
-    def get_episode(self, date: str):
+
+    def get_episode(self, date: str) -> dict:
         """
         Retrieve episode data for a specific date of the given TV show.
 
@@ -121,4 +122,3 @@ class EpisodeService:
         response["Episodes"].append(episode)
 
         return response
-
